61881944420afab7f03d3b11dfb22b2013830e33
vvdpzz <vvdpzz@gmail.com>
2011-09-21 20:10:12 +0800

    Cube burn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cube::Application.config.secret_token = 'e3bc83d4e776439cdbaba6dfa4ca4da2b79e526e45bb98e609863c9741f39deb619e240f50c03fe98b074e97f1414d0b639c6eb39fdd4f331eef3a1759ae5d3a'
