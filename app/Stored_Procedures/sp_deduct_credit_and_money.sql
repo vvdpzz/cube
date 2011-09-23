bc8c87f320e44ff6171f1fa7072ab2c31b00d20e
xxd <xxdxxd51@hotmail.com>
2011-09-23 14:17:34 +0800

    sp

---
+++ app/Stored_Procedures/sp_deduct_credit_and_money.sql
@@ -0,0 +1,97 @@
+/*Usage: 
+1 create this stored procedure in your database
+2 Usage in MySQL: mysql> call sp_deduct_credit_and_money ('test','test',1,0,10.00)
+3 Usage in Rails: ActiveRecord::Base.connection.execute("call sp_deduct_credit_and_money('#{title}','#{content}',#{id},#{credit},#{money})") @question_controller.rb
+*/
+/* Drop if Exists */
+DROP PROCEDURE IF EXISTS sjobs_development.sp_deduct_credit_and_money
+
+/* Crete Stored Procedure*/
+DELIMITER //
+CREATE PROCEDURE sp_deduct_credit_and_money (
+	in uuid int,
+	in user_id int,
+	in title varchar(100),
+	in content text,
+	in deduct_credit int,
+	in deduct_money DECIMAL(8,2))
+BEGIN
+IF deduct_credit = 0 AND deduct_money =0.00 THEN
+	START TRANSACTION;
+ 	/* Create a new question */
+	INSERT INTO questions (id, answers_count, content, correct_answer_id, created_at, credit, money, title, updated_at, user_id, votes_count) 
+	VALUES              (uuid, 0, content, 0, NOW(), deduct_credit, deduct_money, title, NOW(), user_id, 0);
+	
+	/* update user info*/
+	UPDATE users
+   		SET questions_count = COALESCE(questions_count, 0) + 1,
+			     updated_at = NOW(), 
+			  	     credit = credit - deduct_credit
+ 	  WHERE users.id = user_id;
+	
+	COMMIT;
+ELSE IF deduct_credit > 0 AND deduct_money =0.00 THEN
+	START TRANSACTION;
+ 	/* Create a new question */
+	INSERT INTO questions (id, answers_count, content, correct_answer_id, created_at, credit, money, title, updated_at, user_id, votes_count) 
+	VALUES (uuid, 0, content, 0, NOW(), deduct_credit, deduct_money, title, NOW(), user_id, 0);
+
+	/* update user info*/
+	UPDATE users
+   		SET questions_count = COALESCE(questions_count, 0) + 1,
+			 updated_at = NOW(), 
+			 credit = credit - deduct_credit
+ 	  WHERE users.id = user_id;
+
+	/*insert into tran*/
+	INSERT INTO credit_transactions (answer_id, created_at, payment, question_id, trade_status, trade_type, 	updated_at, user_id, value, winner_id) 
+	VALUES (NULL, NOW(), 1, uuid, 0, 0, NOW(), user_id, deduct_credit, NULL);
+	
+	COMMIT;
+ELSE IF deduct_credit = 0 AND deduct_money > 0.00 THEN
+	START TRANSACTION;
+ 	/* Create a new question */
+	INSERT INTO questions (id, answers_count, content, correct_answer_id, created_at, credit, money, title, updated_at, user_id, votes_count) 
+	VALUES (uuid, 0, content, 0, NOW(), deduct_credit, deduct_money, title, NOW(), user_id, 0);
+
+	/* update user info*/
+	UPDATE users
+   		SET questions_count = COALESCE(questions_count, 0) + 1,
+					  updated_at = NOW(), 
+					  money = money - deduct_money
+ 	  WHERE users.id = user_id;
+
+	/*insert into tran*/
+	INSERT INTO money_transactions (answer_id, created_at, payment, question_id, trade_status, 
+	trade_type, updated_at, user_id, value, winner_id) 
+	VALUES (NULL, now(), 1, uuid, 0, 0, now(), user_id, deduct_money, NULL);
+	
+	COMMIT;
+ELSE IF deduct_credit > 0 AND deduct_money > 0.00 THEN
+	START TRANSACTION;
+ 	/* Create a new question */
+	INSERT INTO questions (id, answers_count, content, correct_answer_id, created_at, credit, money, title, updated_at, user_id, votes_count) 
+	VALUES (uuid, 0, content, 0, NOW(), deduct_credit, deduct_money, title, NOW(), user_id, 0);
+	
+	/* update user info*/
+	UPDATE users
+   		SET questions_count = COALESCE(questions_count, 0) + 1,
+					  updated_at = NOW(), 
+					  credit = credit - deduct_credit,
+					  money = money - deduct_money
+ 	  WHERE users.id = user_id;
+
+	/*insert into tran*/
+	INSERT INTO credit_transactions (answer_id, created_at, payment, question_id, trade_status, trade_type, 	updated_at, user_id, value, winner_id) 
+	VALUES (NULL, NOW(), 1, uuid, 0, 0, NOW(), user_id, deduct_credit, NULL);
+	
+	INSERT INTO money_transactions (answer_id, created_at, payment, question_id, trade_status, 
+	trade_type, updated_at, user_id, value, winner_id) 
+	VALUES (NULL, now(), 1, uuid, 0, 0, now(), user_id, deduct_money, NULL);
+	
+	COMMIT;
+end if;
+end if;
+end if;
+end if;
+END
--- db/migrate/20110831092045_create_questions.rb
+++ db/migrate/20110831092045_create_questions.rb
@@ -1,7 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions, :id => false do |t|
-      t.bigint :id
+      t.integer :id, :limit => 8, :null => false
       t.references :user, :null => false
       t.string :title, :null => false
       t.text :content
