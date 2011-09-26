cc64ecc2be4435241b22812af6194999103354cd
xxd <xxdxxd51@hotmail.com>
2011-09-26 13:50:11 +0800

    create accept answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,33 @@
 class AnswersController < ApplicationController
   # POST /answers
   def create
+    id, user_id, question_id, content, is_correct, vote_count = UUIDList.pop, current_user.id, params[:question_id], params[:answer]
+    answer = Answer.new :id => id, :user_id => user_id, :question_id => question_id, :content => content
+
+    respond_to do |format|
+      if answer.save
+        question = Question.find_by_id id
+        data = question.serializable_hash
+        data.merge! User.basic_hash current_user.id
+        format.json { render :json => data, :status => :created, :location => question }
+      else
+        format.json { render :json => question.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
     
+      if @answer.save
+        question.not_free? and question.correct_answer_id == 0 and @answer.deduct_credit and @answer.order_credit
+        # format.html { redirect_to question, :notice => 'Answer was successfully created.' }
+        question.async_new_answer(@answer.id)
+        format.js
+        format.json { render :json => @answer, :status => :created, :location => [question, @answer] }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @answer.errors, :status => :unprocessable_entity }
+      end
+    end
   end
   
   # PUT /questions/:question_id/answers/:id/accept
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -17,4 +17,14 @@ class Answer < ActiveRecord::Base
       errors.add :credit, t(:credit_not_enough_to_answer_warning)
     end
   end
+  
+  def self.strong_insert(id, title, content, credit, money)
+    sql = ActiveRecord::Base.connection()
+    sql.execute "SET autocommit=0"
+    sql.begin_db_transaction
+    sql.update "update questions set title = #{title}, content = #{content}, credit = #{credit}, money = #{money} where id = #{id}";
+    sql.update "update questions set title = #{title}, content = #{content}, credit = #{credit}, money = #{money} where id = #{id}";
+    sql.commit_db_transaction
+  end
+  
 end
