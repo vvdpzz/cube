c55695135aee4c0af318702521c157f9797050b7
vvdpzz <vvdpzz@gmail.com>
2011-10-13 12:26:04 +0800

    ug

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -44,7 +44,7 @@ class AnswersController < ApplicationController
       new_comments = old_comments + ',' + MultiJson.encode(hash)
     end
     
-    Notification.notif_new_a_comment (instance.user_id,answer_id)
+    # Notification.notif_new_a_comment (instance.user_id,answer_id)
     respond_to do |format|
       Answer.strong_update_comment(instance.id,new_comments)
       format.json { render :json => hash }
@@ -68,7 +68,7 @@ class AnswersController < ApplicationController
     if question_info.user_id != current_user.id and question_info.correct_answer_id == 0
       Question.strong_accept_answer(question_id, answer_id, user_id, credit, money)
       question.async_accept_answer(answer.id)
-      Notification.notif_answer_accepted (question_id, answer_id)
+      # Notification.notif_answer_accepted (question_id, answer_id)
       respond_to do |format|
         format.json { head :ok }
       end
